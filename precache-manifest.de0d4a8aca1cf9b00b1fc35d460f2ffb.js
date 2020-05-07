d61ea0c2d4a46d169fb71cd2b721f2924cd36f24
yashg160 <yashg160@gmail.com>
2020-05-07 18:00:30 +0530

    Updates

---
+++ precache-manifest.de0d4a8aca1cf9b00b1fc35d460f2ffb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4073a21eae4389da42eb614b0f12744d",
+    "url": "/Reader/index.html"
+  },
+  {
+    "revision": "19e1af85cfc7c1ba8b11",
+    "url": "/Reader/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b869a1dc9f2baa4c86f6",
+    "url": "/Reader/static/js/2.e51f1dae.chunk.js"
+  },
+  {
+    "revision": "19e1af85cfc7c1ba8b11",
+    "url": "/Reader/static/js/main.67bfbc5b.chunk.js"
+  },
+  {
+    "revision": "9ec229de7a5504b80cbb",
+    "url": "/Reader/static/js/runtime-main.62425433.js"
+  }
+]);
